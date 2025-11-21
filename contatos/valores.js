38436f0d211acf31ee11e629268dcce711486dad
meg <mgmymd@outlook.com>
2025-11-20 22:51:00 -0300

    primeiro teste utilizando arquivo js no maestro

---
+++ contatos/valores.js
@@ -0,0 +1,17 @@
+// Valores para criar contato
+
+output.contatos = {
+  contato1: {
+    nome: "Melissa",
+    telefone: "999999999",
+    telefone_visível: "(999) 999-999",
+    email: "fake@email.com",
+    endereco: "Jiufen"
+  },
+  contato2: {
+    nome: "Beatriz",
+    telefone: "999999999",
+    telefone_visível: "(999) 999-999",
+    email: "fake2@email2.com",
+  },
+};
